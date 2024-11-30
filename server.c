49b0b27c8fb0197fea47206537a31566e86232f8
kall37 <rimalsubash77@gmail.com>
2024-11-30 08:00:00 +0545

    refactor: httpclient to parse useragent headers

--- server.c
+++ server.c
@@ -33,6 +33,7 @@ int main() {
   struct sockaddr_in client_addr, serv_addr;
   socklen_t client_addr_len = sizeof(client_addr);
   char buffer[BUFFER_SIZE];
+  char buffercpy[BUFFER_SIZE];
 
   server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd == -1) {
@@ -70,7 +71,9 @@ int main() {
     }
     memset(buffer, 0, BUFFER_SIZE);
     read(client_fd, buffer, BUFFER_SIZE);
-    printf("Message from client : %s", buffer);
+    strcpy(buffercpy, buffer);
+
+    printf("Message from client : %s \n", buffer);
 
     /* TOKENIZERS */
     char *first_method = strtok(buffer, "/");
@@ -94,9 +97,25 @@ int main() {
         send(client_fd, echo_string_type, BUFFER_SIZE, 0);
       } else if (strcmp(second_str, " HTTP") == 0) {
         send(client_fd, normal_response, 40, 0);
+      } else if (strstr(buffercpy, "user-agent")) {
+        char *user_agent = strstr(buffercpy, "User-Agent: ");
+        if ((!user_agent)) {
+          continue;
+        }
+        user_agent += strlen("User-Agent: ");
+        char *eof = strstr(user_agent, "\r\n");
+        if (eof) {
+          *eof = '\0';
+        }
+        sprintf(echo_string_type,
+                "HTTP/1.1 200 OK\r\nContent-Type: "
+                "text/plain\r\nContent-Length: %lu\r\n\r\n%s",
+                strlen(user_agent), user_agent);
+        send(client_fd, echo_string_type, BUFFER_SIZE, 0);
       } else {
-        send(client_fd, notfound, 40, 0);
+        send(client_fd, notfound, sizeof(notfound), 0);
       }
+
     } else {
       printf("Response to client failed : %s", strerror(errno));
     }
